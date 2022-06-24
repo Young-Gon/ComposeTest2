5b17f66836874db9ca556ef5cc1b31947e200eb6
gondev <yg1027@gmail.com>
2022-06-24 14:59:18 +0900

    commit test

--- app/src/main/java/com/gondev/myapplication/ui/login/LoginScreen.kt
+++ app/src/main/java/com/gondev/myapplication/ui/login/LoginScreen.kt
@@ -2,12 +2,13 @@ package com.gondev.myapplication.ui.login
 
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.navigation.NavBackStackEntry
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
 import androidx.navigation.navigation
 
 @Composable
-fun LoginScreen() {
+fun LoginScreen(navBackStackEntry: NavBackStackEntry) {
     Text(text = "PROFILE")
 }
 
@@ -18,8 +19,7 @@ fun NavGraphBuilder.loginNavGraph() {
         startDestination = "login/login"
     ) {
         composable("login/login") {
-
-            LoginScreen()
+            LoginScreen(it)
         }
     }
 }
